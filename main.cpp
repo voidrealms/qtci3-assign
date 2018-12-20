d7fcf4d2d0e6c8e30f92dd03ec2675822c54c0ec
Bryan Cairns <bcairns@voidrealms.com>
2018-12-20 09:55:21 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QSettings>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QSettings settings;
+    settings.setValue("key", 123);
+
+    settings.beginGroup("test");
+    settings.setValue("key", 456);
+    settings.endGroup();
+
+    settings.sync();
+
+    qInfo() << settings.value("key");
+
+    settings.beginGroup("test");
+    qInfo() << settings.value("key");
+    settings.endGroup();
+
+    return a.exec();
+}
